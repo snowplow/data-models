4e7eeef094bf24b604baa1322290054269e3689a
colmsnowplow <colm@snowplowanalytics.com>
2020-12-04 18:58:35 +0000

    Sessions - Add sessions module

--- web/v1/bigquery/sql-runner/sql/standard/03-sessions/01-main/03-sessions.sql
+++ web/v1/bigquery/sql-runner/sql/standard/03-sessions/01-main/03-sessions.sql
@@ -1,7 +1,23 @@
+/*
+   Copyright 2020-2021 Snowplow Analytics Ltd. All rights reserved.
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
 CREATE OR REPLACE TABLE {{.scratch_schema}}.sessions_this_run{{.entropy}}
+CLUSTER BY domain_userid,domain_sessionid
 AS (
   SELECT
-
     -- app ID
     a.app_id,
 
@@ -140,9 +156,3 @@ AS (
   INNER JOIN {{.scratch_schema}}.sessions_lasts{{.entropy}} c
     ON b.domain_sessionid = c.domain_sessionid
 );
-
-
-
-CREATE OR REPLACE TABLE {{.scratch_schema}}.sessions_staged{{.entropy}} AS(SELECT * FROM {{.scratch_schema}}.sessions_this_run{{.entropy}});
-
-CREATE OR REPLACE TABLE {{.output_schema}}.sessions{{.entropy}} AS(SELECT * FROM {{.scratch_schema}}.sessions_this_run{{.entropy}});
